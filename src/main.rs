4f7f244b45ec39a6fa42ee3f22fec133df82f9d8
Bryan Nguyen <31323380+bdangnguyen@users.noreply.github.com>
2025-03-02 23:16:44 -0800

    Create simple REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_prompt();
+        let input_buffer = read_input();
+
+        match input_buffer.as_str().trim() {
+            ".exit" => std::process::exit(0),
+            _ => println!("Unrecognized command: {}", input_buffer)
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("rdb > ");
+    std::io::stdout().flush().unwrap();
+}
+
+fn read_input() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    return input;
 }
