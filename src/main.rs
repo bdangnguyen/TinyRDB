067e49287952854927063986d34776efcb10b4e6
Bryan Nguyen <31323380+bdangnguyen@users.noreply.github.com>
2025-03-05 00:14:22 -0800

    Add naive parsing for SELECT and INSERT

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,60 @@
-use std::io::Write;
+use std::{io::Write, os::macos::raw::stat};
+
+enum MetaCommand {
+    Exit,
+    Unknown(String)
+}
+
+impl From<&str> for MetaCommand {
+    fn from(input_buffer: &str) -> Self {
+        match input_buffer.trim() {
+            ".exit" => Self::Exit,
+            _ => Self::Unknown(input_buffer.to_string())
+        }
+    } 
+}
+
+enum StatementType {
+    Insert,
+    Select,
+    Unknown(String)
+}
+
+struct Statement {
+    statement_type: StatementType
+}
+
+impl From<&str> for Statement {
+    fn from(input_buffer: &str) -> Self {
+        let statement_type = match input_buffer {
+            input_buffer if input_buffer.starts_with("insert") => StatementType::Insert,
+            input_buffer if input_buffer.starts_with("select") => StatementType::Select,
+            _ => StatementType::Unknown(input_buffer.to_string())
+        };
+
+        return Statement {
+            statement_type
+        }
+    }
+}
 
 fn main() {
     loop {
         print_prompt();
-        let input_buffer = read_input();
+        let input_buffer: String = read_input();
 
-        match input_buffer.as_str().trim() {
-            ".exit" => std::process::exit(0),
-            _ => println!("Unrecognized command: {}", input_buffer)
+        if input_buffer.chars().next().eq(&Some('.')) {
+            let meta_command: MetaCommand = MetaCommand::from(input_buffer.as_str());
+            match meta_command {
+                MetaCommand::Exit => std::process::exit(0),
+                MetaCommand::Unknown(command) => {
+                    print!("Unrecognized command: {}", command);
+                    continue;
+                }
+            };
         }
+
+        let statement = handle_statement(input_buffer.as_str());
     }
 }
 
@@ -22,4 +68,14 @@ fn read_input() -> String {
     std::io::stdin().read_line(&mut input).unwrap();
 
     return input;
+}   
+
+fn handle_statement(input_buffer: &str) {
+    let statement: Statement = Statement::from(input_buffer);
+
+    match statement.statement_type {
+        StatementType::Insert =>  println!("Handling Insert"),
+        StatementType::Select => println!("Handling Select"),
+        StatementType::Unknown(statement) => print!("Unrecognized keyword at start of: {}", statement)
+    }
 }
